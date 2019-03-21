9c32c255dfd3e620268e20313ab70c4f7c2e79ff
scbedd <45376673+scbedd@users.noreply.github.com>
2019-03-20 18:31:20 -0700

    starting to check the packages

--- eng/check_packages.ps1
+++ eng/check_packages.ps1
@@ -1,21 +1,40 @@
 # retrieve the information for each of the passed packages
-
 param (
   $artifactLocation
 )
 
+function verifyPackageWheel($wheel)
+{
+    Write-Host $wheel
+}
 
+function verifyPackageSDist($tar)
+{
+    Write-Host $tar
+}
 
-#foreach ( $p in(Get-ChildItem $pathToArtifacts -Recurse -Filter "*.tgz" -File) ){
-#    try{
-#        Write-Host "npm publish $($p.FullName) --access=$accessLevel --registry=$registry --always-auth=true $tag"
-#        npm publish $p.FullName --access=$accessLevel --registry=$registry --always-auth=true $tag
-#    }
-#    catch{
-#        $ErrorMessage = $_.Exception.Message
-#        Write-Host $ErrorMessage
-#    }
-#}
+# wheels
+foreach ($wheel in (Get-ChildItem $artifactLocation\* -Recurse -Include *.whl))
+{
+   try 
+   {
+        verifyPackageWheel($wheel)
+   }
+   catch 
+   {
+        Write-Host $_.Exception.Message
+   }
+}
 
-
-
+# sdist
+foreach ($tar in (Get-ChildItem $artifactLocation\* -Recurse -Include *.tar.gz))
+{
+   try 
+   {
+        verifyPackageSDist($tar)
+   }
+   catch 
+   {
+        Write-Host $_.Exception.Message
+   }
+}
