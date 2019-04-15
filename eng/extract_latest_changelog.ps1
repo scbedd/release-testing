27936c3785ad832fb17107cd3c0687a5f5ab1093
scbedd <45376673+scbedd@users.noreply.github.com>
2019-04-15 11:26:11 -0700

    getting super close. need to  update the version matching regex

--- eng/extract_latest_changelog.ps1
+++ eng/extract_latest_changelog.ps1
@@ -2,35 +2,82 @@
 function ExtractReleaseNotes($changeLogLocation)
 {
   $releaseNotes = @{}
+  $contentArrays = @{}
   if ($changeLogLocation.Length -eq 0)
   {
     return $releaseNotes
   }
 
-  $contents = Get-Content -Raw $changeLogLocation
-  $noteMatches = Select-String $RELEASE_NOTE_REGEX -input $contents -AllMatches | % { $_.matches }
+  $contents = Get-Content $changeLogLocation
 
-  foreach($releaseNoteMatch in $noteMatches)
-  {
-    $version = $releaseNoteMatch.Groups['version'].Value
-    $text = $releaseNoteMatch.Groups['releaseText'].Value
-
-    $releaseNotes[$version] = New-Object PSObject -Property @{
-      ReleaseContent = $text
+  $version = ''
+  foreach($line in $contents){
+    if ($line -match $RELEASE_TITLE_REGEX)
+    {
+      $version = $matches['version']
+      $contentArrays[$version] = @()
+      $contentArrays[$version] += $line
+      $releaseNotes[$version] = New-Object PSObject -Property @{
+        ReleaseContent = ''
+        ReleaseVersion = '$version'
+      }
     }
+    else {
+      $contentArrays[$version] += $line
+    }
+  }
+
+  # $contents = Get-Content -Raw $changeLogLocation
+  # $noteMatches = Select-String $RELEASE_NOTE_REGEX -input $contents -AllMatches | % { $_.matches }
+
+  # foreach($releaseNoteMatch in $noteMatches)
+  # {
+  #   $version = $releaseNoteMatch.Groups['version'].Value
+  #   $text = $releaseNoteMatch.Groups['releaseText'].Value
+
+  #   $releaseNotes[$version] = New-Object PSObject -Property @{
+  #     ReleaseContent = $text
+  #   }
+  # }
+
+  foreach($val in $releaseNotes)
+  {
+    $val.Value.ReleaseContent = $contentArrays[$val.Value.ReleaseVersion] -join [Environment]::NewLine
   }
 
   return $releaseNotes
 }
 
+
+function CheckLines($changeLogLocation)
+{
+  Write-Host $TITLE_REGEX
+  $contents = Get-Content $changeLogLocation
+
+  foreach($line in $contents)
+  {
+    if($line -match $TITLE_REGEX)
+    {
+      Write-Host $line
+      Write-Host $matches['version']
+    }
+  }
+}
+
 $RELEASE_NOTE_REGEX = "(?<releaseNote>\#\s(?<releaseDate>[\-0-9]+)\s-\s(?<version>(\d+)(\.(\d+))?(\.(\d+))?(([^0-9][^\s]+)))(?<releaseText>((?!\s# )[\s\S])*))"
 $VERSION_REGEX = "(\d+)(\.(\d+))?(\.(\d+))?(([^0-9][^\s]+))"
+$RELEASE_TITLE_REGEX = "(?<releaseNoteTitle>\#(?<preVersion>[\s]*?)(?<version>(\d+)(\.(\d+))?(\.(\d+))?(([^0-9][^\s]+))))"
 $SAMPLE_CHANGELOG_LOCATION = "C:/repo/sdk-for-js/sdk/servicebus/service-bus/changelog.md"
+$UNCAPTURED_VERSION_REGEX = "((\d+)(\.(\d+))?(\.(\d+))?(([^0-9][^\s]+)))"
+$CAPTURED_VERSION_REGEX = "(?<version>(\d+)(\.(\d+))?(\.(\d+))?(([^0-9][^\s]+)))"
 
-$a = ExtractReleaseNotes -changeLogLocation $SAMPLE_CHANGELOG_LOCATION
+
+$NEGATIVE_LOOKAHEAD_REGEX = "((?!$UNCAPTURED_VERSION_REGEX)[\s\S])*"
+$TITLE_REGEX = "(?<releaseNoteTitle>^\#\s$NEGATIVE_LOOKAHEAD_REGEX$CAPTURED_VERSION_REGEX)"
 
 
-
+#ExtractReleaseNotes -changeLogLocation $SAMPLE_CHANGELOG_LOCATION
+CheckLines -changeLogLocation $SAMPLE_CHANGELOG_LOCATION
 
 # https://stackoverflow.com/questions/12572164/multiline-regex-to-match-config-block
 
@@ -49,3 +96,4 @@ $a = ExtractReleaseNotes -changeLogLocation $SAMPLE_CHANGELOG_LOCATION
 # (?<releaseNote>\#\s(?<releaseDate>[\-0-9]+)\s(?<version>(\d+)(\.(\d+))?(\.(\d+))?(([^0-9][^\s]+)))(?<releaseText>(.*?)\#\ ))
 # (?<releaseNote>\#\s(?<releaseDate>[\-0-9]+)\s(?<version>(\d+)(\.(\d+))?(\.(\d+))?(([^0-9][^\s]+)))(?<releaseText>(.*?)\#\ ))
 
+#(?<releaseNoteTitle>\#([\s]*?)(?<version>(\d+)(\.(\d+))?(\.(\d+))?(([^0-9][^\s]+))))
